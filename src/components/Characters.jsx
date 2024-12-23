63580bc951ce4aabb4254b1e5356461453e9e65b
Julio Castaño <julio_ccatb@hotmail.com>
2024-12-22 22:50:49 -0400

    text green

--- src/components/Characters.jsx
+++ src/components/Characters.jsx
@@ -1,28 +1,25 @@
-import { characters } from "../../data";
-
+import { characters } from '../../data';
 
 const Characters = () => {
-
-
-  return (
-    <section id="characters" className="character-section">
-      <div className=""></div>
-      <h1>Characters</h1>
-      <div className="character-container">
-        <ul className="character-grid">
-          {characters.map((character) => {
-            return (
-              <li key={character.id} className="character">
-                <a href={character.href}>
-                  <img src={character.img} alt={character.name} />
-                  <h2>{character.name}</h2>
-                </a>
-              </li>
-            );
-          })}
-        </ul>
-      </div>
-    </section>
-  );
+	return (
+		<section id='characters' className='character-section'>
+			<div className=''></div>
+			<h1 className='text-green-500'>Characters</h1>
+			<div className='character-container'>
+				<ul className='character-grid'>
+					{characters.map((character) => {
+						return (
+							<li key={character.id} className='character'>
+								<a href={character.href}>
+									<img src={character.img} alt={character.name} />
+									<h2>{character.name}</h2>
+								</a>
+							</li>
+						);
+					})}
+				</ul>
+			</div>
+		</section>
+	);
 };
 export default Characters;
